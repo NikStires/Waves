cefd556966cd5d59c5dd26c0f65507b8e1e0011c
Nik Stires <nik.stires@gmail.com>
2020-11-18 12:32:39 -0800

    inital commit

---
+++ src/components/LibrarySong.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+const LibrarySong = ({
+  song,
+  setCurrentSong,
+  songs,
+  id,
+  audioRef,
+  isPlaying,
+  setSongs,
+}) => {
+  const songSelectHandler = async () => {
+    await setCurrentSong(song);
+    //add active state
+    const newSongs = songs.map((song) => {
+      if (song.id === id) {
+        return {
+          ...song,
+          active: true,
+        };
+      } else {
+        return {
+          ...song,
+          active: false,
+        };
+      }
+    });
+    setSongs(newSongs);
+    if (isPlaying) audioRef.current.play();
+  };
+  return (
+    <div
+      onClick={songSelectHandler}
+      className={`library-song ${song.active ? 'selected' : ''}`}
+    >
+      <img alt={song.name} src={song.cover} />
+      <div className='song-description'>
+        <h3>{song.name}</h3>
+        <h4>{song.artist}</h4>
+      </div>
+    </div>
+  );
+};
+
+export default LibrarySong;
